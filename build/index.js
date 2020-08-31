605bbc55a29c493a2af14986896f08f441ffe150
Richard Forjoe <richard.forjoe@sky.uk>
2020-08-31 03:57:12 +0100

    added apiMocker setup

--- build/index.js
+++ build/index.js
@@ -1,2 +1,10 @@
 "use strict";
 console.log('Hello world!');
+var apiMocker = require('apimocker');
+//////////////////////////////////////////////////////////////////////////////////
+// Mock api config - sets path of input files
+//////////////////////////////////////////////////////////////////////////////////
+var apiMockerConfigJsonPath = process.cwd() + "/src/how-to-setup-api-mocker/apimocker/apimocker.json";
+var mocker = apiMocker.createServer({ quiet: true }).setConfigFile(apiMockerConfigJsonPath);
+mocker.start(null);
+mocker.stop();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 console.log('Hello world!')
+
+const apiMocker = require('apimocker')
+
+//////////////////////////////////////////////////////////////////////////////////
+// Mock api config - sets path of input files
+//////////////////////////////////////////////////////////////////////////////////
+const apiMockerConfigJsonPath = `${process.cwd()}/src/how-to-setup-api-mocker/apimocker/apimocker.json`;
+const mocker = apiMocker.createServer({ quiet: true }).setConfigFile(apiMockerConfigJsonPath);
+
+mocker.start(null);
+mocker.stop();
