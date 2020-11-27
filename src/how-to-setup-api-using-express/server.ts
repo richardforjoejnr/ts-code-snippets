f60bcbde9975785d5c174b216150f60230812c21
Richard Forjoe <richard.forjoe@sky.uk>
2020-11-27 21:41:50 +0000

    initial changes

--- src/how-to-setup-api-using-express/server.ts
+++ src/how-to-setup-api-using-express/server.ts
@@ -1,23 +1,19 @@
 import express from 'express';
 import * as http from 'http';
-import { startPolling } from '../how-to-setup-dynamic-api-using-express/apiDynamicPollingService'
+import { startPolling } from './dynamic-rest-api/apiDynamicPollingService'
+import {handlePollingEndpoint} from './dynamic-rest-api/handlePollingEndpoint'
+import {handleToken} from './graphql-endpoint/handleToken'
+import {handleData} from './graphql-endpoint/handleData'
 
 
 let server: any;
-
-const handleTimeline = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const id = req.query.id;
-    switch (id) {
-        case 'TEST':
-            res.sendFile('mocks/polledResponse.json',{root: __dirname});
-            break;
-    }
-};
+const enablePolling = false;
 
 const logUrl = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log(`===== FETCHING URL =====> ${req.originalUrl}`);
     next();
 };
+
 export const start = async (): Promise<any> => {
     const app = express();
     const port = '8055';
@@ -26,7 +22,11 @@ export const start = async (): Promise<any> => {
 
     // app.use('/mocks', express.static('./mocks'));
     app.all('*', logUrl);
-    app.get('/endpoint', handleTimeline);
+    app.get('/endpoint', handlePollingEndpoint);
+
+    // GRAPHQL MOCKS - Token + Query
+    app.post('/token', handleToken);
+    app.post('/Data', handleData);
     server.listen(port, () => {
         console.info(`[Mock Server] Listening on Port: ${port}`);
         Promise.resolve();
@@ -42,7 +42,7 @@ export const start = async (): Promise<any> => {
     });
     // Start polling - number of items & polling interval in ms
     // endpoint: http://localhost:8055/endpoint?id=TEST
-    await startPolling(10,5000);
+    (enablePolling) ? await startPolling(10,5000) : {};
 };
 
 export const stop = async (): Promise<any> => {
