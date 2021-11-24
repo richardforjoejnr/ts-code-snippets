96c9df383b470a91fca319c2899686fc7e79e05d
Richard Forjoe <richard.forjoe@sky.uk>
2021-11-24 15:53:08 +0000

    chore(automation): reporting poc clean up

--- cypress/fixtures/reportingMetaData.js
+++ cypress/fixtures/reportingMetaData.js
@@ -6,3 +6,11 @@ export const homePageInfoPageLoadData = {
   c40: 'mobile website',
   c1: 'homepage',
 }
+
+export const surveyPageInfoPageLoadData = {
+  pageName: 'web|myglammxo|surveypage',
+  v63: 'web|myglammxo|surveypage',
+  v66: 'https://m.myglamm.com/myglammxo-survey',
+  c40: 'mobile website',
+  c1: 'surveypage',
+}
